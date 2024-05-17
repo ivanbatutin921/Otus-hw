ba7c2e12b456dd25784c6b72fc5ccc239b9f1276
dopamine <ivanbatutin921@mail.ru>
2024-05-17 22:42:45 +0500

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,19 +2,37 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
+	var b strings.Builder
+	var count int
+	var err error
+
 	if str == "" {
 		return "", nil
 	}
-	if str[0] < '0' || str[0] > '9' {
-		return "", nil
-	}
+
 	for i := 0; i < len(str); i++ {
-		
+		if str[i] >= '0' && str[i] <= '9' {
+			return "", ErrInvalidString
+		}
+
+		if i+1 < len(str) && str[i+1] >= '0' && str[i+1] <= '9' {
+			count, err = strconv.Atoi(string(str[i+1]))
+			if err != nil {
+				return "", err
+			}
+			b.WriteString(strings.Repeat(string(str[i]), count))
+			i++
+		} else {
+			b.WriteString(string(str[i]))
+		}
 	}
-	return "", nil
+
+	return b.String(), nil
 }
