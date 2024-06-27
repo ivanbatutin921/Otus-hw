fedca97e105ace74ef673035e25b90e001d970d1
dopamine <ivanbatutin921@mail.ru>
2024-06-27 23:57:12 +0500

    23:55||27.06/2024

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,46 +10,45 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 func Run(tasks []Task, n, m int) error {
+	if m <= 0 {
+		return ErrErrorsLimitExceeded
+	}
+
 	taskCh := make(chan Task)
 	errorCh := make(chan error)
 	var errorCount int
 	var wg sync.WaitGroup
 
-	// запускаем n горутин для выполнения задач
+	go func() {
+		for _, task := range tasks {
+			taskCh <- task
+		}
+		close(taskCh)
+	}()
+
 	for i := 0; i < n; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			for task := range taskCh {
 				err := task()
-				if err!= nil {
+				if err != nil {
 					errorCh <- err
-					return
+					//return nil
 				}
 			}
 		}()
 	}
 
-	// запускаем горутину для принятия ошибок и подсчета их количества
-	wg.Add(1)
 	go func() {
-		defer wg.Done()
 		for err := range errorCh {
-			_ = err
 			errorCount++
 			if errorCount >= m {
 				close(errorCh)
-				close(taskCh)
+				break
 			}
+			_ = err
 		}
-	}()
-
-	// отправляем задачи в канал
-	go func() {
-		for _, task := range tasks {
-			taskCh <- task
-		}
-		close(taskCh)
 	}()
 
 	wg.Wait()
