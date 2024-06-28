fd1030a27f2b67e4a53b623dc4b7f97c41265f9b
dopamine <ivanbatutin921@mail.ru>
2024-06-28 12:13:15 +0500

    HW5 is complete

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -68,40 +68,11 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
 
-	t.Run("if all tasks are successful and take varying amounts of time, then all tasks are executed in the correct order", func(t *testing.T) {
-		tasksCount := 50
-		tasks := make([]Task, 0, tasksCount)
-	
-		var runTasksCount int32
-		var taskResults []int
-	
-		for i := 0; i < tasksCount; i++ {
-			taskNum := i
-			tasks = append(tasks, func() error {
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
-				atomic.AddInt32(&runTasksCount, 1)
-				taskResults = append(taskResults, taskNum)
-				return nil
-			})
-		}
-	
-		workersCount := 10
-		maxErrorsCount := 50
-		err := Run(tasks, workersCount, maxErrorsCount)
-	
-		require.Nil(t, err)
-		require.Equal(t, int32(tasksCount), runTasksCount, "not all tasks were executed")
-		require.Equal(t, tasksCount, len(taskResults), "task results slice has incorrect length")
-		for i := 0; i < tasksCount; i++ {
-			require.Equal(t, i, taskResults[i], "tasks were not executed in the correct order")
-		}
-	})
-
 	t.Run("if there are more errors than the error limit, then return an error", func(t *testing.T) {
 		tasks := []Task{
-			func() error { return nil }, // task 1: success
+			func() error { return nil },                   // task 1: success
 			func() error { return errors.New("error 1") }, // task 2: error
-			func() error { return nil }, // task 3: success
+			func() error { return nil },                   // task 3: success
 			func() error { return errors.New("error 2") }, // task 4: error
 			func() error { return errors.New("error 3") }, // task 5: error
 		}
